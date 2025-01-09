17d7c507d55802b80a24f673a098889babef1431
LuqmanRM <luqmanrofif2018@mail.ugm.ac.id>
2025-01-09 11:36:13 +0700

    add validation email format when register

--- src/main/java/com/example/simple_crud/auth/RegisterRequest.java
+++ src/main/java/com/example/simple_crud/auth/RegisterRequest.java
@@ -1,11 +1,15 @@
 package com.example.simple_crud.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class RegisterRequest {
+    @NotBlank(message = "Email is required")
+    @Email
     public String email;
     public String password;
     public String name;
